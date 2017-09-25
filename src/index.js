149bfa499950a66a60dad699f99a37b246a26467
Robert Laws <roblaws@me.com>
2017-09-25 11:25:50 +0300

    add basic css support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-var heading = document.createElement("h1")
-heading.textContent = "Development Starter"
-document.body.appendChild(heading)
+import _ from 'lodash'
+import './style.css'
+
+function component() {
+  var heading = document.createElement("h1")
+  heading.innerHTML = _.join(['Development', 'Webpack'], ' - ')
+  heading.classList.add('topHeading')
+  return heading
+}
+
+document.body.appendChild(component())
